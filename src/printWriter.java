53af3e5f363f8844c5863c30f546b97c0f9291fa
rawda1234 <108896808+rawda1234@users.noreply.github.com>
2022-12-27 18:09:25 +0200

    Add files via upload

---
+++ src/printWriter.java
@@ -0,0 +1,19 @@
+
+import java.io.PrintWriter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rawda
+ */
+public class printWriter extends PrintWriter {
+
+    public printWriter(String string) {
+    }
+    
+}
